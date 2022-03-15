0d1310a3ee59250dec7e9150f3ce3b2c385c1d3e
rvasquezm <rodrigovasquez225@gmail.com>
2022-03-15 15:15:33 -0600

    avance semana 6-Tarea 3

--- ProyectoJerson.py
+++ ProyectoJerson.py
@@ -4,10 +4,10 @@ from venv import main
 
 main = tk.Tk()
 main.title("Proyecto de Compiladores")
-
-
-
-
+main.columnconfigure(0 ,weight=1)
+main.rowconfigure(0 ,weight=2)
+main.rowconfigure(1, minsize=2)
+main.rowconfigure(2, weight=1)
 
 #Funcion para colocar texto en cuadro
 def llenar_Texto(lineas):
@@ -19,7 +19,11 @@ def llenar_Texto(lineas):
 def abrir_Archivo():
     url = fd.askopenfilename(title = "Abrir Archivos",filetypes=(("Archivos de Texto","*.txt"),("Todos los Archivos","*.*")))
     file = open(url,'r')
-    llenar_Texto(file.read())
+    lines=''
+    for line in file:
+        lines+=line
+    #llenar_Texto(file.read())
+    llenar_Texto(lines)
     file.close()
     
 
@@ -34,8 +38,28 @@ menu_Archivo.add_command(label="Abrir",command = abrir_Archivo)
 # Cuadro de texto
 
 Texto = tk.Text(main)
-Texto.grid(row = 0, column = 0, sticky = 'nswe')
+Texto.grid(row = 0, column = 0, sticky = 'nswe',padx = 10, pady =5)
+
+#Label 
+Titulo=tk.Label(main, text='Manejador de Errores')
+Titulo.grid(row = 1, column = 0, sticky = 'w', padx = 10)
 
 
+# Manejador de Errores
+
+Manejador_Errores = tk.Text(main,height =10)
+Manejador_Errores.config(state=tk.DISABLED)
+Manejador_Errores.grid(row = 2, column = 0, sticky = 'nswe',padx = 10, pady = 5)
+
+#Scroll para el manejador de errores
+scrollV = tk.Scrollbar(main, command=Manejador_Errores.yview)
+scrollV.grid(row=2, column=1, sticky="nsew")
+
+#Scroll para el cuadro de texto
+scrollVCuadroTexto = tk.Scrollbar(main, command=Texto.yview)
+scrollVCuadroTexto.grid(row=0, column=1, sticky="nsew")
+
+Texto.config(yscrollcommand=scrollVCuadroTexto.set)
+Manejador_Errores.config(yscrollcommand=scrollV.set)
 main.config(menu=menu_Principal)
 main.mainloop()
